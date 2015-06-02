0ab1eb928a730e119a5a470e844bc293bf3a61a7
Sashko Stubailo <sashko@stubailo.com>
2015-06-01 19:37:46 -0700

    Build a react-runtime package that loads dev and prod

---
+++ react-runtime-dev/package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'react-runtime-dev',
+  version: '0.13.3',
+  // Brief, one-line summary of the package.
+  summary: 'Development version of the React runtime library with addons.',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md',
+  debugOnly: true
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('react-0.13.3.js');
+
+  // So that this package loads after the minified version and overwrites it
+  api.use('react-runtime-prod');
+});
---
+++ react-runtime-prod/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'react-runtime-prod',
+  version: '0.13.3',
+  // Brief, one-line summary of the package.
+  summary: 'Production version of the React runtime library with addons.',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('react-0.13.3.min.js');
+});
