e65b879a9f35072adb232b35c78731f74cc2e2d3
Avital Oliver <avital@thewe.net>
2015-06-23 14:50:58 -0700

    Remove commented-out test

--- packages/react-template-helper/tests.jsx
+++ packages/react-template-helper/tests.jsx
@@ -27,36 +27,6 @@ Tinytest.add(
     fails(Template.UsesEmptyComponentTemplateWithSiblingTextNode);
   });
 
-
-/**
-  Test disabled: The following detection may not be possible at
-  run-time. When React renders a component it clears its container
-  element, so any Blaze marker nodes used to know where to insert a new
-  element are lost. We get a "Failed to execute 'removeChild' on 'Node':
-  The node to be removed is not a child of this node."
-
-Tinytest.add(
-  "react-template-helper-tests - rendering react component with conditional sibling gives helpful error",
-  function (test) {
-    var tmpl = Template.UsesEmptyComponentTemplateWithConditionalSibling;
-    var sibling = new ReactiveVar(false);
-    tmpl.helpers({
-      sibling() {
-        return sibling.get();
-      }
-    });
-
-    // shouldn't throw
-    renderToDiv(tmpl);
-    Tracker.flush({_throwFirstError: true});
-
-    test.throws(function () {
-      sibling.set(true);
-      Tracker.flush({_throwFirstError: true});
-    }, /EmptyComponentTemplateWithoutContainerElement.*EmptyReactComponent.*only child/);
-  });
-*/
-
 Tinytest.add(
   "react-template-helper-tests - pass props into `{{> React}}`",
   function (test) {
