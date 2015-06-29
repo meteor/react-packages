bf9976833d75c0d39c6d00198add434005bb2131
Sashko Stubailo <sashko@stubailo.com>
2015-06-28 21:56:57 -0700

    Update react package dependency

--- packages/react/package.js
+++ packages/react/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'react',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Everything you need to use React with Meteor.',
   // URL to the Git repository containing the source code for this package.
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
 
   api.imply([
     'jsx@0.1.0',
-    'react-runtime@0.13.3',
+    'react-runtime@0.13.3_1',
     'react-meteor-data@0.1.0'
   ]);
 });
