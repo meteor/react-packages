8cd1f7a6916bc9b77f3bb7a3316f018b3d312ce2
David Greenspan <dgreenspan@alum.mit.edu>
2015-07-10 16:48:41 -0700

    New published versions for babel packages
    I just published:
    * babel-runtime 0.1.1.
    * babel-compiler 5.6.15.
    * jsx 0.1.1.
    * babel-tests 0.1.1.

--- packages/babel-tests/package.js
+++ packages/babel-tests/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babel-tests',
   summary: "Tests for the babel package",
-  version: '0.1.0',
+  version: '0.1.1',
   documentation: 'README.md'
 });
 
@@ -14,9 +14,9 @@ Package.onTest(function (api) {
   api.versionsFrom('METEOR@1.1.0.2');
   api.use('tinytest');
   api.use('underscore');
-  api.use('babel-compiler@5.4.7', 'server');
+  api.use('babel-compiler@5.6.15', 'server');
   api.use('babel-tests');
-  api.use('jsx@0.1.0');
+  api.use('jsx@0.1.1');
 
   // Tests that call the transpiler (which is only possible on the server)
   // and look at the result.
--- packages/jsx/package.js
+++ packages/jsx/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "jsx",
   summary: "Build plugin that transpiles .jsx files using Babel",
-  version: '0.1.0',
+  version: '0.1.1',
   documentation: 'README.md'
 });
 
 Package.registerBuildPlugin({
   name: 'transpileJSX',
-  use: ['babel-compiler@5.4.7'],
+  use: ['babel-compiler@5.6.15'],
   sources: [
     'jsx-plugin.js'
   ]
@@ -15,5 +15,5 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
   // We need the Babel helpers as a run-time dependency of the generated code.
-  api.imply('babel-runtime@0.1.0');
+  api.imply('babel-runtime@0.1.1');
 });
