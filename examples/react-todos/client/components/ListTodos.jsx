c7d15e22c5f9194ebd8b066aea4c50cf94a585ec
Giant Elk <flyinghorsedancing@gmail.com>
2015-07-11 19:42:16 -0400

    Fixed indent.

--- examples/react-todos/client/components/ListTodos.jsx
+++ examples/react-todos/client/components/ListTodos.jsx
@@ -6,25 +6,25 @@ ListTodos = React.createClass({
     var self = this;
 
     var  showTodoList = this.props.lists.map(function (list) {
-            var className = "list-todo";
-            if (self.props.getListId() === list._id) {
-              className += " active";
-            }
+      var className = "list-todo";
+      if (self.props.getListId() === list._id) {
+        className += " active";
+      }
 
-            return (
-              <Link
-                className={ className }
-                key={ list._id }
-                to="todoList"
-                params={{ listId: list._id }}>
-                  { list.name }
-                  { list.incompleteCount ?
-                    <span className="count-list">
-                      { list.incompleteCount }
-                    </span> : "" }
-              </Link>
-            );
-          });
+      return (
+        <Link
+          className={ className }
+          key={ list._id }
+          to="todoList"
+          params={{ listId: list._id }}>
+            { list.name }
+            { list.incompleteCount ?
+              <span className="count-list">
+                { list.incompleteCount }
+              </span> : "" }
+        </Link>
+      );
+    });
 
     return (
       <div>
