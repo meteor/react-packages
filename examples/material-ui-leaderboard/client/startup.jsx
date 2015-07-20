005e0b952ab4d7128d0358d2bdab23a00712927a
Evan You <yyx990803@gmail.com>
2015-07-20 16:22:30 -0400

    use correct webfonts (Roboto)

--- examples/material-ui-leaderboard/client/startup.jsx
+++ examples/material-ui-leaderboard/client/startup.jsx
@@ -1,19 +1,5 @@
 Meteor.startup(function () {
-  var WebFontConfig = {
-    google: { families: [ 'Roboto:400,300,500:latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
-
   injectTapEventPlugin();
-
   $(document.body).html("<div id='container'></div>");
   React.render(<App />, document.getElementById("container"));
 });
