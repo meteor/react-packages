21cfcbfe7cabeb30bc8e5b0077205c13424d14c1
David Greenspan <dgreenspan@alum.mit.edu>
2015-08-07 09:24:28 -0700

    Make react-runtime refer to ReactProd indirectly
    This is necessary if react-runtime-prod is prodOnly.

--- packages/react-runtime/react-runtime.js
+++ packages/react-runtime/react-runtime.js
@@ -1,5 +1,8 @@
 if (Package["react-runtime-dev"]) {
   React = Package["react-runtime-dev"].ReactDev;
+} else if (Package["react-runtime-prod"]) {
+  React = Package["react-runtime-prod"].ReactProd;
 } else {
-  React = ReactProd;
+  // not sure how this can happen
+  throw new Error("Couldn't find react-runtime-dev or react-runtime-prod packages");
 }
