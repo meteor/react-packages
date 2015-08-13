adc01ac2b5d3ed0aab2373a679f2b5e4667d91d7
David Greenspan <dgreenspan@alum.mit.edu>
2015-08-12 21:28:04 -0700

    Version 0.1.9 of react package

--- packages/react/package.js
+++ packages/react/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'react',
-  version: '0.1.8',
+  version: '0.1.9',
   // Brief, one-line summary of the package.
   summary: 'Everything you need to use React with Meteor.',
   // URL to the Git repository containing the source code for this package.
