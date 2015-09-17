c679238e56bbcb3513eda35caa3f4ee25341cb4b
Sashko Stubailo <sashko@stubailo.com>
2015-09-16 17:08:22 -0700

    Add hubspot to React docs page

---
+++ docs/analytics.js
@@ -0,0 +1,6 @@
+(function(d,s,i,r) {
+  if (d.getElementById(i)){return;}
+  var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+  n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/520701.js';
+  e.parentNode.insertBefore(n, e);
+})(document,"script","hs-analytics",300000);
