710928d9fd0619b2bec460daf85cd2a0dd40b53a
Sashko Stubailo <sashko@stubailo.com>
2015-09-21 13:11:58 -0400

    Make spurious change

--- packages/react-runtime/package.js
+++ packages/react-runtime/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'react-runtime',
   version: '0.13.3_6',
   // Brief, one-line summary of the package.
-  summary: 'Facebook\'s React library',
+  summary: "Facebook's React library",
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/meteor/react-packages',
   // By default, Meteor will default to using README.md for documentation.
