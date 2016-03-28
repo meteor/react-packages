e2484b0380500d02a954f1403fe2f5a5f35ea9e3
Tom Coleman <tom@thesnail.org>
2016-03-29 09:26:44 +1100

    Allow check-npm-versions to run before requiring

--- packages/react-meteor-data/react-meteor-data.jsx
+++ packages/react-meteor-data/react-meteor-data.jsx
@@ -1,10 +1,10 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
   react: '0.14.x',
-  'react-addons-pure-render-mixin': '0.14.x'
+  'react-addons-pure-render-mixin': '0.14.x',
 });
 
-import createContainer from './createContainer.jsx';
-import ReactMeteorData from './ReactMeteorData.jsx';
+const createContainer = require('./createContainer.jsx').default;
+const ReactMeteorData = require('./ReactMeteorData.jsx').default;
 
 export { createContainer, ReactMeteorData };
