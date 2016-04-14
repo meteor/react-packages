057e1b9fbefbbc724f501ca27c1036329deb9124
Tom Coleman <tom@thesnail.org>
2016-04-14 16:58:33 -0700

    Bump package versions to publish.
    New package versions:
    * react-runtime: 15.0.1
    * react: 15.0.1
    * react-meteor-data: 0.2.9
    * react-template-helper: 0.2.9

--- packages/react-runtime/package.js
+++ packages/react-runtime/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'react-runtime',
-  version: '15.0.0',
+  version: '15.0.1',
   // Brief, one-line summary of the package.
   summary: "Facebook's React library",
   // URL to the Git repository containing the source code for this package.
