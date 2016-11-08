284fbd198c687850c6d6d1c5e3a90b0e8391cff3
Tomas Brambora <tomas.brambora@gmail.com>
2016-11-08 22:00:15 +1100

    Added comments.

--- packages/react-meteor-data/react-meteor-data.d.ts
+++ packages/react-meteor-data/react-meteor-data.d.ts
@@ -1,4 +1,6 @@
 declare module 'meteor/react-meteor-data' {
+  // `T` is external props.
+  // `U` is Meteor container props.
   export const createContainer: <T, U>(
     fun: (props: T) => U,
     comp: React.ComponentClass<U & T> | React.StatelessComponent<U & T>)
