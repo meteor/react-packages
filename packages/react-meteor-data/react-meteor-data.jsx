de11cec374ab605b4ae6bc43e3b03f4ec49903ea
leocavalcante <lc@leocavalcante.com>
2017-04-09 22:43:42 -0300

    Convert ReactMeteorData to a Higher-Order Component

--- packages/react-meteor-data/react-meteor-data.jsx
+++ packages/react-meteor-data/react-meteor-data.jsx
@@ -1,10 +1,9 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+
 checkNpmVersions({
   react: '15.x',
-  'react-addons-pure-render-mixin': '15.x',
 }, 'react-meteor-data');
 
 const createContainer = require('./createContainer.jsx').default;
-const ReactMeteorData = require('./ReactMeteorData.jsx').default;
 
-export { createContainer, ReactMeteorData };
+export { createContainer };
