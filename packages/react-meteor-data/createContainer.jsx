cead57f6a3164ec22e42aff404111c6f057b0e9b
Leo Cavalcante <lc@leocavalcante.com>
2017-05-05 03:51:22 -0300

    Convert ReactMeteorData to a Higher-Order Component (#217)
    * Convert ReactMeteorData to a Higher-Order Component
    
    * Return the Component constructor to avoid breaking current API
    
    * Put back pure component logic
    
    * Use Facebook conventions for HOC's connect
    
    * Remove unused packages and update patch version
    
    * Shorten API export

--- packages/react-meteor-data/createContainer.jsx
+++ packages/react-meteor-data/createContainer.jsx
@@ -2,11 +2,8 @@
  * Container helper using react-meteor-data.
  */
 
-import createReactClass from 'create-react-class';
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-import ReactMeteorData from './ReactMeteorData.jsx';
+import { connect } from './ReactMeteorData.jsx';
 
 export default function createContainer(options = {}, Component) {
   let expandedOptions = options;
@@ -16,25 +13,5 @@ export default function createContainer(options = {}, Component) {
     };
   }
 
-  const {
-    getMeteorData,
-    pure = true,
-  } = expandedOptions;
-
-  const mixins = [ReactMeteorData];
-  if (pure) {
-    mixins.push(PureRenderMixin);
-  }
-
-  /* eslint-disable react/prefer-es6-class */
-  return createReactClass({
-    displayName: 'MeteorDataContainer',
-    mixins,
-    getMeteorData() {
-      return getMeteorData(this.props);
-    },
-    render() {
-      return <Component {...this.props} {...this.data} />;
-    },
-  });
+  return connect(expandedOptions)(Component);
 }
