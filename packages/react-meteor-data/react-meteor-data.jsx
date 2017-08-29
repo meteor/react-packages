fc25460c7a93e88c883531b8619b2d71ca7bc612
David Burles <dburles@gmail.com>
2017-08-29 16:19:49 +1000

    add withTracker to react-meteor-data

--- packages/react-meteor-data/react-meteor-data.jsx
+++ packages/react-meteor-data/react-meteor-data.jsx
@@ -5,4 +5,5 @@ checkNpmVersions({
 }, 'react-meteor-data');
 
 export { default as createContainer } from './createContainer.jsx';
+export { default as withTracker } from './withTracker.jsx';
 export { ReactMeteorData } from './ReactMeteorData.jsx';
