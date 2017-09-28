0b14bc893011a45dd72241aa3714194af50fb4de
Christian Klaussner <developer@klaussner.me>
2017-09-28 15:50:23 +0200

    Update version range for React 15 & 16

--- packages/react-meteor-data/react-meteor-data.jsx
+++ packages/react-meteor-data/react-meteor-data.jsx
@@ -1,7 +1,7 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-  react: '15.x',
+  react: '15.3 - 16',
 }, 'react-meteor-data');
 
 export { default as createContainer } from './createContainer.jsx';
