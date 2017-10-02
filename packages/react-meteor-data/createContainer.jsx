d9f1ef4d1566edad8f06283466e7f0bf9761197e
Levi Thomason <me@levithomason.com>
2017-10-02 12:55:07 -0700

    feat(createContainer): allow function composition

--- packages/react-meteor-data/createContainer.jsx
+++ packages/react-meteor-data/createContainer.jsx
@@ -6,6 +6,9 @@ import React from 'react';
 import { connect } from './ReactMeteorData.jsx';
 
 export default function createContainer(options = {}, Component) {
+  // cheap curry
+  if (Component === undefined) return createContainer.bind(createContainer, options);
+
   let expandedOptions = options;
   if (typeof options === 'function') {
     expandedOptions = {
