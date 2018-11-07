98f1e6a8caf471738d11ecfd7e71d33093cbb53f
Arthur Leonard Andersen <aandersen@velalu.qa>
2018-11-07 16:42:45 +0100

    Check correct version of React

--- packages/react-meteor-hooks/react-meteor-hooks.jsx
+++ packages/react-meteor-hooks/react-meteor-hooks.jsx
@@ -1,7 +1,7 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
-  react: '16.7',
+  react: '16.7.0-alpha.0',
 }, 'react-meteor-data');
 
 export { default as useMeteorSubscription } from './useMeteorSubscription.jsx';
