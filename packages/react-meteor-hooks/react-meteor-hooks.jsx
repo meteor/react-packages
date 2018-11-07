726b84c335682370a9dc1c0d682511f4420b963e
Arthur Leonard Andersen <aandersen@velalu.qa>
2018-11-07 16:53:44 +0100

    Fix `checkNpmVersions` package reference

--- packages/react-meteor-hooks/react-meteor-hooks.jsx
+++ packages/react-meteor-hooks/react-meteor-hooks.jsx
@@ -2,7 +2,7 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
   react: '16.7.0-alpha.0',
-}, 'react-meteor-data');
+}, 'react-meteor-hooks');
 
 export { default as useMeteorSubscription } from './useMeteorSubscription.jsx';
 export { default as useMeteorData } from './useMeteorData.jsx';
