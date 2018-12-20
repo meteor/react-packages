031729d9d6291cf42f5b28b7ead8e0521b2015f5
Adrian Lanning <alanning@gmail.com>
2018-12-20 13:05:35 -0500

    support dynamic import via lazy loading

--- packages/react-template-helper/package.js
+++ packages/react-template-helper/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'react-template-helper',
-  version: '0.2.11',
+  version: '0.2.12',
   // Brief, one-line summary of the package.
   summary: 'Use React components in native Meteor templates',
   // URL to the Git repository containing the source code for this package.
@@ -20,5 +20,5 @@ Package.onUse(function(api) {
     'tmeasday:check-npm-versions@0.3.2'
   ]);
 
-  api.addFiles(['react-template-helper.js'], 'client');
+  api.addFiles(['react-template-helper.js'], 'client', {lazy: true});
 });
