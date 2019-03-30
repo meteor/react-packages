ae3f32381e7b06c8444cb0c943c4caf0e9a7fff0
Yves Chedemois <y.chedemois@gmail.com>
2019-03-30 21:11:59 +0100

    Add a note about refs BC break and #262

--- packages/react-meteor-data/withTracker.jsx
+++ packages/react-meteor-data/withTracker.jsx
@@ -6,6 +6,8 @@ export default function withTracker(options) {
     const expandedOptions = typeof options === 'function' ? { getMeteorData: options } : options;
     const { getMeteorData, pure = true } = expandedOptions;
 
+    // Note : until https://github.com/meteor/react-packages/pull/266 is merged (which forwards ref to the inner Component),
+    // moving from a class to a function component will break existing code giving refs to withTracker-decorated components.
     function WithTracker(props) {
       const data = useTracker(() => getMeteorData(props) || {}, [props]);
       return <Component {...props} {...data} />;
