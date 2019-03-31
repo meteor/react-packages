61ef31ede4c6bda0eb18909b951712b5e1a22cff
Yves Chedemois <y.chedemois@gmail.com>
2019-03-31 14:51:24 +0200

    minor: streamline comments

--- packages/react-meteor-data/useTracker.js
+++ packages/react-meteor-data/useTracker.js
@@ -37,12 +37,18 @@ if (Meteor.isServer) {
 else {
   // @todo specify a default value for dependencies ? Omitting them can be very bad perf-wise.
   useTracker = (reactiveFn, dependencies) => {
-    // Run the function once on mount without autorun or subscriptions,
-    // to get the initial return value.
-    // Note: maybe when React Suspense is officially available we could
-    // throw a Promise instead to skip the 1st render altogether ?
+    // Note : we always run the reactiveFn in Tracker.nonreactive in case
+    // we are already inside a Tracker Computation. This can happen if someone calls
+    // `ReactDOM.render` inside a Computation. In that case, we want to opt out
+    // of the normal behavior of nested Computations, where if the outer one is
+    // invalidated or stopped, it stops the inner one too.
+
     const [trackerData, setTrackerData] = useState(() => {
-      // We need to prevent subscriptions from running in that initial run.
+      // No side-effects are allowed when computing the initial value.
+      // To get the initial return value for the 1st render on mount,
+      // we run reactiveFn without autorun or subscriptions.
+      // Note: maybe when React Suspense is officially available we could
+      // throw a Promise instead to skip the 1st render altogether ?
       const realSubscribe = Meteor.subscribe;
       Meteor.subscribe = () => ({ stop: () => {}, ready: () => false });
       const initialData = Tracker.nonreactive(reactiveFn);
@@ -52,11 +58,6 @@ else {
 
     useEffect(() => {
       let computation;
-      // Use Tracker.nonreactive in case we are inside a Tracker Computation.
-      // This can happen if someone calls `ReactDOM.render` inside a Computation.
-      // In that case, we want to opt out of the normal behavior of nested
-      // Computations, where if the outer one is invalidated or stopped,
-      // it stops the inner one.
       Tracker.nonreactive(() => {
         computation = Tracker.autorun(() => {
           const data = reactiveFn();
