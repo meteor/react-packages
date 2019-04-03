17322a554b50ac50281eac999df1bee0cc99643d
Yves Chedemois <y.chedemois@gmail.com>
2019-04-03 21:58:52 +0200

    remove dependency on tmeasday:check-npm-versions

--- packages/react-meteor-data/index.js
+++ packages/react-meteor-data/index.js
@@ -1,8 +1,11 @@
-import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+import React from 'react';
 
-checkNpmVersions({
-  react: '16.8',
-}, 'react-meteor-data');
+if (Meteor.isDevelopment) {
+  const v = React.version.split('.');
+  if (v[0] < 16 || v[1] < 8) {
+    console.warn('react-meteor-data 2.x requires React version >= 16.8.');
+  }
+}
 
 export { default as withTracker } from './withTracker.jsx';
 export { default as useTracker } from './useTracker.js';
