0c1dd3c4835e89d7248a666f5f79d8eea0d43bc3
Kevin Newman <Kevin@unFocus.com>
2019-07-19 15:40:29 -0400

    Test changes to enclosed values when not using deps too

--- packages/react-meteor-data/useTracker.tests.js
+++ packages/react-meteor-data/useTracker.tests.js
@@ -7,20 +7,19 @@ import useTracker from './useTracker';
 
 Tinytest.add('useTracker - no deps', async function (test) {
   const reactiveDict = new ReactiveDict();
-  reactiveDict.setDefault('key', 'initial')
   let runCount = 0;
 
   const { result, rerender, unmount, waitForNextUpdate } = renderHook(
-    () => useTracker(() => {
+    ({ name }) => useTracker(() => {
       runCount++;
-      return reactiveDict.get('key');
-    })
+      reactiveDict.setDefault(name, 'initial');
+      return reactiveDict.get(name);
+    }),
+    { initialProps: { name: 'key' } }
   );
 
   test.equal(result.current, 'initial', 'Expect initial value to be "initial"');
   test.equal(runCount, 1, 'Should have run 1 times');
-
-  if (Meteor.isServer) return;
 
   act(() => reactiveDict.set('key', 'changed'));
   await waitForNextUpdate();
@@ -29,18 +28,25 @@ Tinytest.add('useTracker - no deps', async function (test) {
   test.equal(runCount, 2, 'Should have run 2 times');
 
   rerender();
+  await waitForNextUpdate();
 
   test.equal(result.current, 'changed', 'Expect value of "changed" to persist after rerender');
   test.equal(runCount, 3, 'Should have run 3 times');
 
+  rerender({ name: 'different' });
+  await waitForNextUpdate();
+
+  test.equal(result.current, 'default', 'After deps change, the default value should have returned');
+  test.equal(runCount, 4, 'Should have run 4 times');
+
   unmount();
-  test.equal(runCount, 3, 'Unmount should not cause a tracker run');
+  test.equal(runCount, 4, 'Unmount should not cause a tracker run');
 
   act(() => reactiveDict.set('different', 'changed again'));
   // we can't use await waitForNextUpdate() here because it doesn't trigger re-render - is there a way to test that?
 
   test.equal(result.current, 'default', 'After unmount, changes to the reactive source should not update the value.');
-  test.equal(runCount, 3, 'After unmount, useTracker should no longer be tracking');
+  test.equal(runCount, 4, 'After unmount, useTracker should no longer be tracking');
 
   reactiveDict.destroy();
 });
@@ -60,8 +66,6 @@ Tinytest.add('useTracker - with deps', async function (test) {
 
   test.equal(result.current, 'default', 'Expect the default value for given name to be "default"');
   test.equal(runCount, 1, 'Should have run 1 times');
-
-  if (Meteor.isServer) return;
 
   act(() => reactiveDict.set('name', 'changed'));
   await waitForNextUpdate();
