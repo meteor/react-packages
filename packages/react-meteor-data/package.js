574416a6ff0d00c5a789faa783b3bb644f909c23
Kevin Newman <Kevin@unFocus.com>
2019-07-19 14:59:57 -0400

    Add some basic tests for useTracker

--- packages/react-meteor-data/package.js
+++ packages/react-meteor-data/package.js
@@ -1,3 +1,5 @@
+/* global Package */
+
 Package.describe({
   name: 'react-meteor-data',
   summary: 'React higher-order component for reactively tracking Meteor data',
@@ -6,10 +8,16 @@ Package.describe({
   git: 'https://github.com/meteor/react-packages',
 });
 
-Package.onUse(function (api) {
+Package.onUse((api) => {
   api.versionsFrom('1.3');
   api.use('tracker');
   api.use('ecmascript');
 
   api.mainModule('index.js');
+});
+
+Package.onTest((api) => {
+  api.use(['ecmascript', 'reactive-dict', 'tracker', 'tinytest']);
+  api.use('react-meteor-data');
+  api.mainModule('tests.js');
 });
