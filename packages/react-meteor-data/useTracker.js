5d73c1a9d5e9840aee55807656ba7458d6f3dfd4
Kevin Newman <Kevin@unFocus.com>
2019-07-22 19:04:30 -0400

    Make "falsy" deps check in computation consistent with other checks.

--- packages/react-meteor-data/useTracker.js
+++ packages/react-meteor-data/useTracker.js
@@ -117,7 +117,8 @@ function useTracker(reactiveFn, deps) {
           runReactiveFn();
         } else {
           // If deps are falsy, stop computation and let next render handle reactiveFn.
-          if (!refs.previousDeps) {
+          if (!refs.previousDeps !== null && refs.previousDeps !== undefined
+            && !Array.isArray(refs.previousDeps)) {
             dispose();
           } else {
             runReactiveFn();
