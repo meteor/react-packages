e8784e0675b2dcefde4f4971b48fbfe684273d1b
Kevin Newman <Kevin@unFocus.com>
2019-10-11 13:29:21 -0400

    Run reactiveFn on the server inside nonreactive

--- packages/react-meteor-data/useTracker.js
+++ packages/react-meteor-data/useTracker.js
@@ -180,7 +180,7 @@ function useTrackerClient(reactiveFn, deps, computationHandler) {
 // When rendering on the server, we don't want to use the Tracker.
 // We only do the first rendering on the server so we can get the data right away
 const useTracker = Meteor.isServer
-  ? (reactiveFn) => reactiveFn()
+  ? (reactiveFn) => Tracker.nonreactive(reactiveFn)
   : useTrackerClient;
 
 export default Meteor.isDevelopment
