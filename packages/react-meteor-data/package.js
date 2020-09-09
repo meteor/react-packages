7e4ddb478d182281abb2508a7eb57b03cf896138
filipenevola <filipenevola@gmail.com>
2020-09-09 12:52:10 -0400

    Bump react-meteor-data version to 2.1.1

--- packages/react-meteor-data/package.js
+++ packages/react-meteor-data/package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'react-meteor-data',
   summary: 'React hook for reactively tracking Meteor data',
-  version: '2.1.1',
+  version: '2.1.2',
   documentation: 'README.md',
   git: 'https://github.com/meteor/react-packages',
 });
