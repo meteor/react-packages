f59dd3992e148dfa37f0182691ffd7818397944a
Kevin Newman <kevin@moonfarmer.com>
2020-11-20 01:31:23 -0500

    Make useCursor memoize fetch results
    Uses useReducer to maintain a document level immutable array for changes to the record set.

--- packages/react-mongo/react-mongo.ts
+++ packages/react-mongo/react-mongo.ts
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { Tracker } from 'meteor/tracker'
 import { EJSON } from 'meteor/ejson'
-import { useEffect, useMemo, useReducer, useRef, DependencyList } from 'react'
+import { useEffect, useMemo, useReducer, useRef, DependencyList, Reducer } from 'react'
 
 const fur = (x: number): number => x + 1
 const useForceUpdate = () => useReducer(fur, 0)[1]
@@ -82,29 +82,79 @@ export const useSubscription = (name?: string, ...args: any[]) => (
     : useSubscriptionClient(name, ...args)
 )
 
+type useCursorActions<T> =
+  | { type: 'refresh', data: T[] }
+  | { type: 'addedAt', document: T, atIndex: number }
+  | { type: 'changedAt', document: T, atIndex: number }
+  | { type: 'removedAt', atIndex: number }
+  | { type: 'movedTo', fromIndex: number, toIndex: number }
+
+const useCursorReducer = <T>(data: T[], action: useCursorActions<T>):T[] => {
+  switch (action.type) {
+    case 'refresh':
+      return action.data
+    case 'addedAt':
+      return [
+        ...data.slice(0, action.atIndex),
+        action.document,
+        ...data.slice(action.atIndex)
+      ]
+    case 'changedAt':
+      return [
+        ...data.slice(0, action.atIndex),
+        action.document,
+        ...data.slice(action.atIndex + 1)
+      ]
+    case 'removedAt':
+      return [
+        ...data.slice(0, action.atIndex),
+        ...data.slice(action.atIndex + 1)
+      ]
+    case 'movedTo':
+      const doc = data[action.fromIndex]
+      const copy = [
+        ...data.slice(0, action.fromIndex),
+        ...data.slice(action.fromIndex + 1)
+      ]
+      copy.splice(action.toIndex, 0, doc)
+      return copy
+  }
+}
+
 const useCursorClient = <T = any>(factory: () => Mongo.Cursor<T>, deps: DependencyList = []) => {
   const cursor = useMemo<Mongo.Cursor<T>>(() => Tracker.nonreactive(factory), deps)
-  const forceUpdate = useForceUpdate()
+  const [data, dispatch] = useReducer<Reducer<T[], useCursorActions<T>>, T[]>(useCursorReducer, [], () => cursor.fetch())
 
   useEffect(() => {
-    const observer = cursor.observeChanges({
-      added: forceUpdate,
-      changed: forceUpdate,
-      removed: forceUpdate
+    const observer = cursor.observe({
+      addedAt (document, atIndex, before) {
+        dispatch({ type: 'addedAt', document, atIndex })
+      },
+      changedAt (newDocument, oldDocument, atIndex) {
+        dispatch({ type: 'changedAt', document: newDocument, atIndex })
+      },
+      removedAt (oldDocument, atIndex) {
+        dispatch({ type: 'removedAt', atIndex })
+      },
+      movedTo (document, fromIndex, toIndex, before) {
+        dispatch({ type: 'movedTo', fromIndex, toIndex })
+      },
+      // @ts-ignore
+      _suppress_initial: true
     })
 
     // an update may have happened between first render and commit
-    forceUpdate()
+    dispatch({ type: 'refresh', data: cursor.fetch() })
 
     return () => {
       observer.stop()
     }
   }, [cursor])
 
-  return cursor
+  return data
 }
 
-const useCursorServer = <T = any>(factory: () => Mongo.Cursor<T>) => Tracker.nonreactive(factory)
+const useCursorServer = <T = any>(factory: () => Mongo.Cursor<T>) => Tracker.nonreactive(factory).fetch()
 
 export const useCursor = <T = any>(factory: () => Mongo.Cursor<T>, deps: DependencyList = []) => (
   Meteor.isServer
--- packages/react-mongo/types/react-mongo.d.ts
+++ packages/react-mongo/types/react-mongo.d.ts
@@ -2,4 +2,4 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { DependencyList } from 'react';
 export declare const useSubscription: (name?: string, ...args: any[]) => Meteor.SubscriptionHandle;
-export declare const useCursor: <T = any>(factory: () => Mongo.Cursor<T>, deps?: DependencyList) => Mongo.Cursor<T>;
+export declare const useCursor: <T = any>(factory: () => Mongo.Cursor<T>, deps?: DependencyList) => T[];
