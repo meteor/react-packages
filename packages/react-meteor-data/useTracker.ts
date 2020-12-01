fd8e02fef66024a22079aa391a74ec9e115c315c
Kevin Newman <kevin@unfocus.com>
2020-12-01 01:19:36 -0500

    Rework useTrackerWithDeps and others - Fixes an edge case bug where Subscriptions could get lost or thrash - Cleans up typescript defs - Add resubscript tests for with deps variant, and reorg test groupings - Simplifies implentation - Removes undocumented computation handler

--- packages/react-meteor-data/useTracker.ts
+++ packages/react-meteor-data/useTracker.ts
@@ -1,10 +1,10 @@
 declare var Package: any
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
-import { useReducer, useEffect, useRef, useMemo } from 'react';
+import { useReducer, useState, useEffect, useRef, useMemo, DependencyList } from 'react';
 
 // Warns if data is a Mongo.Cursor or a POJO containing a Mongo.Cursor.
-function checkCursor(data: any): void {
+function checkCursor (data: any): void {
   let shouldWarn = false;
   if (Package.mongo && Package.mongo.Mongo && data && typeof data === 'object') {
     if (data instanceof Package.mongo.Mongo.Cursor) {
@@ -29,56 +29,31 @@ function checkCursor(data: any): void {
 // Used to create a forceUpdate from useReducer. Forces update by
 // incrementing a number whenever the dispatch method is invoked.
 const fur = (x: number): number => x + 1;
-const useForceUpdate = (): CallableFunction => {
-  const [, forceUpdate] = useReducer(fur, 0);
-  return forceUpdate;
-}
+const useForceUpdate = () => useReducer(fur, 0)[1];
 
-type ReactiveFn = (c?: Tracker.Computation) => any;
-type ComputationHandler = (c: Tracker.Computation) => () => void | void;
+interface IReactiveFn<T> {
+  <T>(c?: Tracker.Computation): T
+}
 type TrackerRefs = {
-  reactiveFn: ReactiveFn;
-  computationHandler?: ComputationHandler;
-  deps?: Array<any>;
   computation?: Tracker.Computation;
   isMounted: boolean;
-  disposeId?: ReturnType<typeof setTimeout>;
   trackerData: any;
-  computationCleanup?: () => void;
-  trackerCount?: number
 }
 
-// The follow functions were hoisted out of the closure to reduce allocations.
-// Since they no longer have access to the local vars, we pass them in and mutate here.
-const dispose = (refs: TrackerRefs): void => {
-  if (refs.computationCleanup) {
-    refs.computationCleanup();
-    delete refs.computationCleanup;
-  }
+const useTrackerNoDeps = <T = any>(reactiveFn: IReactiveFn<T>) => {
+  const { current: refs } = useRef<TrackerRefs>({
+    isMounted: false,
+    trackerData: null
+  });
+  const forceUpdate = useForceUpdate();
+
+  // Without deps, always dispose and recreate the computation with every render.
   if (refs.computation) {
     refs.computation.stop();
-    refs.computation = null;
+    // @ts-ignore This makes TS think ref.computation is "never" set
+    delete refs.computation;
   }
-};
 
-const runReactiveFn = Meteor.isDevelopment
-  ? (refs: TrackerRefs, c: Tracker.Computation): void => {
-    const data = refs.reactiveFn(c);
-    checkCursor(data);
-    refs.trackerData = data;
-  }
-  : (refs: TrackerRefs, c: Tracker.Computation): void => {
-    refs.trackerData = refs.reactiveFn(c);
-  };
-
-const clear = (refs: TrackerRefs): void => {
-  if (refs.disposeId) {
-    clearTimeout(refs.disposeId);
-    delete refs.disposeId;
-  }
-};
-
-const track = (refs: TrackerRefs, forceUpdate: Function, trackedFn: Function): void => {
   // Use Tracker.nonreactive in case we are inside a Tracker Computation.
   // This can happen if someone calls `ReactDOM.render` inside a Computation.
   // In that case, we want to opt out of the normal behavior of nested
@@ -86,79 +61,18 @@ const track = (refs: TrackerRefs, forceUpdate: Function, trackedFn: Function): v
   // it stops the inner one.
   Tracker.nonreactive(() => Tracker.autorun((c: Tracker.Computation) => {
     refs.computation = c;
-    trackedFn(c, refs, forceUpdate);
-  }));
-};
-
-const doFirstRun = (refs: TrackerRefs, c: Tracker.Computation): void => {
-  // If there is a computationHandler, pass it the computation, and store the
-  // result, which may be a cleanup method.
-  if (refs.computationHandler) {
-    const cleanupHandler = refs.computationHandler(c);
-    if (cleanupHandler) {
-      if (Meteor.isDevelopment && typeof cleanupHandler !== 'function') {
-        console.warn(
-          'Warning: Computation handler should return a function '
-          + 'to be used for cleanup or return nothing.'
-        );
-      }
-      refs.computationCleanup = cleanupHandler;
-    }
-  }
-  // Always run the reactiveFn on firstRun
-  runReactiveFn(refs, c);
-}
-
-const tracked = (c: Tracker.Computation, refs: TrackerRefs, forceUpdate: Function): void => {
-  if (c.firstRun) {
-    doFirstRun(refs, c);
-  } else {
-    if (refs.isMounted) {
-      // Only run the reactiveFn if the component is mounted.
-      runReactiveFn(refs, c);
-      forceUpdate();
-    } else {
-      // If we got here, then a reactive update happened before the render was
-      // committed - before useEffect has run. We don't want to run the reactiveFn
-      // while we are not sure this render will be committed, so we'll dispose of the
-      // computation, and set everything up to be restarted in useEffect if needed.
-      // NOTE: If we don't run the user's reactiveFn when a computation updates, we'll
-      // leave the computation in a non-reactive state - so we need to dispose here
-      // and let useEffect recreate the computation later.
-      dispose(refs);
-      // Might as well clear the timeout!
-      clear(refs);
-    }
-  }
-};
-
-interface useTrackerSignature {
-  (reactiveFn: ReactiveFn, deps?: null | Array<any>, computationHandler?: ComputationHandler): any
-}
-
-const useTrackerNoDeps: useTrackerSignature = (reactiveFn, deps = null, computationHandler) => {
-  const { current: refs } = useRef<TrackerRefs>({
-    reactiveFn,
-    isMounted: false,
-    trackerData: null
-  });
-  const forceUpdate = useForceUpdate();
-
-  refs.reactiveFn = reactiveFn;
-  if (computationHandler) {
-    refs.computationHandler = computationHandler;
-  }
-
-  // Without deps, always dispose and recreate the computation with every render.
-  dispose(refs);
-  track(refs, forceUpdate, (c: Tracker.Computation) => {
     if (c.firstRun) {
-      doFirstRun(refs, c);
+      // Always run the reactiveFn on firstRun
+      const data = reactiveFn(c);
+      if (Meteor.isDevelopment) {
+        checkCursor(data);
+      }
+      refs.trackerData = data;
     } else {
       // For any reactive change, forceUpdate and let the next render rebuild the computation.
       forceUpdate();
     }
-  });
+  }));
 
   // To avoid creating side effects in render with Tracker when not using deps
   // create the computation, run the user's reactive function in a computation synchronously,
@@ -166,7 +80,10 @@ const useTrackerNoDeps: useTrackerSignature = (reactiveFn, deps = null, computat
   if (!refs.isMounted) {
     // We want to forceUpdate in useEffect to support StrictMode.
     // See: https://github.com/meteor/react-packages/issues/278
-    dispose(refs);
+    if (refs.computation) {
+      refs.computation.stop();
+      delete refs.computation;
+    }
   }
 
   useEffect(() => {
@@ -178,80 +95,51 @@ const useTrackerNoDeps: useTrackerSignature = (reactiveFn, deps = null, computat
     forceUpdate();
 
     // stop the computation on unmount
-    return () => dispose(refs);
+    return () =>{
+      refs.computation?.stop();
+    }
   }, []);
 
   return refs.trackerData;
 }
 
-const useTrackerWithDeps: useTrackerSignature = (reactiveFn, deps: Array<any>, computationHandler) => {
-  const { current: refs } = useRef<TrackerRefs>({
-    reactiveFn,
-    isMounted: false,
-    trackerData: null
-  });
-  const forceUpdate = useForceUpdate();
+const useTrackerWithDeps = <T = any>(reactiveFn: IReactiveFn<T>, deps: DependencyList): T => {
+  let [data, setData] = useState<T>();
 
-  // Always have up to date deps and computations in all contexts
-  refs.reactiveFn = reactiveFn;
-  refs.deps = deps;
-  if (computationHandler) {
-    refs.computationHandler = computationHandler;
-  }
-
-  // We are abusing useMemo a little bit, using it for it's deps
-  // compare, but not for it's memoization.
   useMemo(() => {
-    // stop the old one.
-    dispose(refs);
-
-    track(refs, forceUpdate, tracked)
-
-    // Tracker creates side effect in render, which can be problematic in some cases, such as
-    // Suspense or concurrent rendering or if an error is thrown and handled by an error boundary.
-    // We still want synchronous rendering for a number of reasons (see readme). useTracker works
-    // around memory/resource leaks by setting a time out to automatically clean everything up,
-    // and watching a set of references to make sure everything is choreographed correctly.
-    if (!refs.isMounted) {
-      // Components yield to allow the DOM to update and the browser to paint before useEffect
-      // is run. In concurrent mode this can take quite a long time. 1000ms should be enough
-      // in most cases.
-      refs.disposeId = setTimeout(() => {
-        if (!refs.isMounted) {
-          dispose(refs);
-        }
-      }, 1000);
+    // To jive with the lifecycle interplay between Tracker/Subscribe, run the
+    // reactive function in a computation, then stop it, to force flush cycle.
+    const comp = Tracker.nonreactive(
+      () => Tracker.autorun((c: Tracker.Computation) => {
+        if (c.firstRun) data = reactiveFn();
+      })
+    );
+    // To avoid creating side effects in render, stop the computation immediately
+    Meteor.defer(() => { comp.stop() });
+    if (Meteor.isDevelopment) {
+      checkCursor(data);
     }
   }, deps);
 
   useEffect(() => {
-    refs.isMounted = true;
-
-    // Render is committed, clear the dispose timeout
-    clear(refs);
-
-    // If it took longer than 1000ms to get to useEffect, or a reactive update happened
-    // before useEffect, restart the computation and forceUpdate.
-    if (!refs.computation) {
-      // This also runs runReactiveFn
-      track(refs, forceUpdate, tracked);
-      forceUpdate();
+    const computation = Tracker.autorun((c) => {
+      setData(reactiveFn(c));
+    });
+    return () => {
+      computation.stop();
     }
+  }, deps);
 
-    // stop the computation on unmount
-    return () => dispose(refs);
-  }, []);
-
-  return refs.trackerData;
+  return data as T;
 }
 
-const useTrackerClient: useTrackerSignature = (reactiveFn, deps = null, computationHandler) =>
+const useTrackerClient = <T = any>(reactiveFn: IReactiveFn<T>, deps: DependencyList = null): T =>
   (deps === null || deps === undefined || !Array.isArray(deps))
-    ? useTrackerNoDeps(reactiveFn, deps, computationHandler)
-    : useTrackerWithDeps(reactiveFn, deps, computationHandler);
+    ? useTrackerNoDeps(reactiveFn)
+    : useTrackerWithDeps(reactiveFn, deps);
 
-const useTrackerServer: useTrackerSignature = (reactiveFn, deps = null, computationHandler) =>
-  Tracker.nonreactive(reactiveFn);
+const useTrackerServer = <T = any>(reactiveFn: IReactiveFn<T>, deps: DependencyList): T =>
+  Tracker.nonreactive(reactiveFn) as T;
 
 // When rendering on the server, we don't want to use the Tracker.
 // We only do the first rendering on the server so we can get the data right away
@@ -259,26 +147,20 @@ const useTracker = Meteor.isServer
   ? useTrackerServer
   : useTrackerClient;
 
-const useTrackerDev: useTrackerSignature = (reactiveFn, deps = null, computationHandler) => {
+const useTrackerDev = <T = any>(reactiveFn: IReactiveFn<T>, deps: DependencyList): T => {
   if (typeof reactiveFn !== 'function') {
     console.warn(
       'Warning: useTracker expected a function in it\'s first argument '
       + `(reactiveFn), but got type of ${typeof reactiveFn}.`
     );
   }
-  if (deps && !Array.isArray(deps)) {
+  if (!Array.isArray(deps)) {
     console.warn(
       'Warning: useTracker expected an array in it\'s second argument '
       + `(dependency), but got type of ${typeof deps}.`
     );
   }
-  if (computationHandler && typeof computationHandler !== 'function') {
-    console.warn(
-      'Warning: useTracker expected a function in it\'s third argument'
-      + `(computationHandler), but got type of ${typeof computationHandler}.`
-    );
-  }
-  return useTracker(reactiveFn, deps, computationHandler);
+  return useTracker(reactiveFn, deps);
 }
 
 export default Meteor.isDevelopment
