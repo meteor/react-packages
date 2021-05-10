d80a419966c9c325dd5d78415934bfa33d820151
Jan Dvorak <storyteller@freedombase.net>
2021-05-10 15:48:26 +0200

    Published react-meteor-data@2.3.1

--- packages/react-meteor-data/package.js
+++ packages/react-meteor-data/package.js
@@ -3,12 +3,12 @@
 Package.describe({
   name: 'react-meteor-data',
   summary: 'React hook for reactively tracking Meteor data',
-  version: '2.3.0',
+  version: '2.3.1',
   documentation: 'README.md',
   git: 'https://github.com/meteor/react-packages',
 });
 
-Package.onUse(function (api) {
+Package.onUse((api) => {
   api.versionsFrom(['1.8.2', '1.12', '2.0']);
   api.use('tracker');
   api.use('ecmascript');
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.mainModule('index.js', ['client', 'server'], { lazy: true });
 });
 
-Package.onTest(function (api) {
+Package.onTest((api) => {
   api.use(['ecmascript', 'typescript', 'reactive-dict', 'reactive-var', 'tracker', 'tinytest', 'underscore', 'mongo']);
   api.use('test-helpers');
   api.use('react-meteor-data');
