f5feaad5a0a84965077a74bdfcfc6e37dd26826a
denihs <denilsonh2014@gmail.com>
2022-02-23 17:25:18 -0400

    - Changing package name so we can publish it as a core package
    - updating package name in the README.md

--- packages/react-meteor-accounts/package.js
+++ packages/react-meteor-accounts/package.js
@@ -1,7 +1,7 @@
 /* global Package */
 
 Package.describe({
-  name: 'react-meteor-accounts',
+  name: 'mdg:react-meteor-accounts',
   summary: 'React hooks and HOCs for reactively tracking Meteor Accounts data',
   version: '1.0.0',
   documentation: 'README.md',
