74d0b574a3f5ec09b3d26fce1c583b6a2022ae56
hschmaiske <ishenriquealbert@gmail.com>
2022-11-26 14:13:10 -0300

    fix: named exports for useTracker and withTracker

--- packages/react-meteor-data/index.js
+++ packages/react-meteor-data/index.js
@@ -1,14 +1,16 @@
 /* global Meteor*/
-import React from 'react';
+import React from 'react'
 
 if (Meteor.isDevelopment) {
-  const v = React.version.split('.');
+  const v = React.version.split('.')
   if (v[0] < 16 || (v[0] == 16 && v[1] < 8)) {
-    console.warn('react-meteor-data 2.x requires React version >= 16.8.');
+    console.warn('react-meteor-data 2.x requires React version >= 16.8.')
   }
 }
 
-export { default as useTracker } from './useTracker';
-export { default as withTracker } from './withTracker.tsx';
-export { useFind } from './useFind';
-export { useSubscribe } from './useSubscribe';
+import useTracker from './useTracker'
+import withTracker from './withTracker.tsx'
+export { useFind } from './useFind'
+export { useSubscribe } from './useSubscribe'
+
+export { useTracker, withTracker }
