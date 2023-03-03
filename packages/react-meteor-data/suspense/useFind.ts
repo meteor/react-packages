83e7440b3c75fdd8758d91c4bb5b5fd162e51a34
Gabriel Grubba <Grubba27@hotmail.com>
2023-03-03 17:39:55 -0300

    feat: upgrading to use suspense

---
+++ packages/react-meteor-data/suspense/useFind.ts
@@ -0,0 +1,126 @@
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+import { useEffect } from 'react'
+import isEqual from 'lodash/isEqual';
+import clone from 'lodash/clone'
+
+
+export const selectorsCache = new Map<Mongo.Collection<unknown>, SelectorEntry[]>();
+type SelectorEntry = {
+  findArgs: Parameters<Mongo.Collection<unknown>['find']>;
+  promise: Promise<unknown>;
+  result?: unknown;
+  error?: unknown;
+  counter: number;
+
+};
+export const removeFromArray =
+  <T>(list: T[], obj: T): void => {
+    if (obj) {
+      const index = list.indexOf(obj);
+      if (index !== -1) {
+        list.splice(index, 1);
+      }
+    }
+  }
+const useFindSuspense = <T = any>(
+  collection: Mongo.Collection<T>,
+  findArgs: Parameters<Mongo.Collection<T>['find']> | null,
+) => {
+  useEffect(() => {
+    const cachedSelectors = selectorsCache.get(collection) ?? [];
+    const _args = clone(findArgs)
+    const selector = cachedSelectors.find(x => isEqual(x.findArgs, _args));
+
+    if (selector) ++selector.counter;
+
+    return () => {
+      // defer
+      setTimeout(() => {
+        const cachedSelectors = selectorsCache.get(collection) ?? [];
+        const selector = cachedSelectors.find(x => isEqual(x.findArgs, _args));
+        if (selector && --selector.counter === 0) {
+          removeFromArray(cachedSelectors, selector);
+          selectorsCache.set(collection, cachedSelectors);
+        }
+      }, 0)
+    };
+  }, [findArgs, collection]);
+
+  useEffect(() => {
+    return () => {
+      // defer
+      setTimeout(() => {
+        const cachedSelectors = selectorsCache.get(collection) ?? [];
+        if (
+          findArgs === null && cachedSelectors.length > 0
+        ) {
+          for (const selector of cachedSelectors) {
+            removeFromArray(cachedSelectors, selector);
+            selectorsCache.set(collection, cachedSelectors);
+          }
+        }
+      }, 0)
+    }
+  }, [findArgs])
+
+  if (findArgs === null) return null
+
+  const cachedSelectors = selectorsCache.get(collection) ?? [];
+  const cachedSelector = cachedSelectors.find(x => isEqual(x.findArgs, findArgs));
+
+  if (cachedSelector) {
+    if ('error' in cachedSelector) {
+      throw cachedSelector.error;
+    }
+    if ('result' in cachedSelector) {
+      return cachedSelector.result as T[];
+    }
+    throw cachedSelector.promise;
+  }
+
+
+  const selector: SelectorEntry = {
+    findArgs,
+    promise: collection
+      .find(...findArgs)
+      .fetchAsync()
+      .then(
+        result => {
+          selector.result = result
+        },
+        error => {
+          selector.error = error;
+        }),
+    counter: 0,
+  };
+  cachedSelectors.push(selector);
+  selectorsCache.set(collection, cachedSelectors);
+
+  throw selector.promise;
+};
+
+export { useFindSuspense }
+
+export const useFind = useFindSuspense
+
+function useFindDev<T = any>(
+  collection: Mongo.Collection<T>,
+  findArgs: Parameters<Mongo.Collection<T>['find']> | null) {
+  function warn(expects: string, pos: string, arg: string, type: string) {
+    console.warn(
+      `Warning: useFind expected a ${ expects } in it\'s ${ pos } argument `
+      + `(${ arg }), but got type of \`${ type }\`.`
+    );
+  }
+
+  if (typeof collection !== 'object') {
+    warn("Mongo Collection", "1st", "reactiveFn", collection);
+  }
+
+  return useFindSuspense(collection, findArgs);
+}
+
+export default Meteor.isDevelopment
+  ? useFindDev
+  : useFind;
---
+++ packages/react-meteor-data/suspense/useSubscribe.ts
@@ -0,0 +1,66 @@
+import { useEffect } from 'react';
+import { EJSON } from 'meteor/ejson';
+import { Meteor } from 'meteor/meteor';
+
+import isEqual from 'lodash/isEqual';
+import { removeFromArray } from "./useFind";
+
+const cachedSubscriptions: SubscriptionEntry[] = [];
+type SubscriptionEntry = {
+  params: EJSON[];
+  name: string;
+  handle?: Meteor.SubscriptionHandle;
+  promise: Promise<unknown>;
+  result?: unknown;
+  error?: unknown;
+};
+
+export function useSubscribeSuspense(name: string, ...params: EJSON[]) {
+  const cachedSubscription = cachedSubscriptions.find(x => x.name === name && isEqual(x.params, params));
+
+  useEffect(() => {
+    return () => {
+      setTimeout(() => {
+        if (cachedSubscription) {
+          cachedSubscription?.handle?.stop();
+          removeFromArray(cachedSubscriptions, cachedSubscription);
+        }
+      }, 0)
+    }
+  }, [name, ...params])
+
+  if (cachedSubscription) {
+    if ('error' in cachedSubscription) {
+      throw cachedSubscription.error;
+    }
+    if ('result' in cachedSubscription) {
+      return cachedSubscription.result;
+    }
+    throw cachedSubscription.promise;
+  }
+
+  const subscription: SubscriptionEntry = {
+    name,
+    params,
+    promise: new Promise<void>((resolve, reject) => {
+      const h = Meteor.subscribe(name, ...params, {
+        onReady() {
+          subscription.result = null;
+          subscription.handle = h
+          resolve();
+        },
+        onStop(error: unknown) {
+          subscription.error = error;
+          subscription.handle = h
+          reject();
+        },
+      });
+    }),
+  };
+
+  cachedSubscriptions.push(subscription);
+
+  throw subscription.promise;
+}
+
+export const useSubscribe = useSubscribeSuspense
