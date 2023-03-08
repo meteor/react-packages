8ac185b90d28980ad5d92edd7052dea10935f90e
Gabriel Grubba <Grubba27@hotmail.com>
2023-03-08 16:34:46 -0300

    chore: updated types to match updated api

--- packages/react-meteor-data/suspense/react-meteor-data.d.ts
+++ packages/react-meteor-data/suspense/react-meteor-data.d.ts
@@ -3,6 +3,6 @@ import { type Mongo } from 'meteor/mongo'
 import { type EJSON } from 'meteor/ejson'
 import { type Meteor } from 'meteor/meteor'
 
-export function useFind<T> (collection: Mongo.Collection<T>, findArgs: Parameters<Mongo.Collection<T>['find']> | null): T[] | null
+export function useFind<T> (key: string, collection: Mongo.Collection<T>, findArgs: Parameters<Mongo.Collection<T>['find']> | null): T[] | null
 
 export function useSubscribe (name: string, ...params: EJSON[]): Meteor.SubscriptionHandle
