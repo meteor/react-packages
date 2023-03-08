60ec938cca5e04ced8ee4785c3e9df1297ddd731
Gabriel Grubba <Grubba27@hotmail.com>
2023-03-08 16:34:04 -0300

    chore: linted and removed unsued types in .d.ts

--- packages/react-meteor-data/react-meteor-data.d.ts
+++ packages/react-meteor-data/react-meteor-data.d.ts
@@ -1,37 +1,43 @@
-import * as React from 'react';
-import { Mongo } from 'meteor/mongo';
-import { EJSON } from "meteor/ejson";
-import { Meteor } from "meteor/meteor";
+import type * as React from 'react'
+import { type Mongo } from 'meteor/mongo'
 
-export function useTracker<TDataProps>(reactiveFn: () => TDataProps): TDataProps
-export function useTracker<TDataProps>(reactiveFn: () => TDataProps, deps: React.DependencyList): TDataProps;
-export function useTracker<TDataProps>(
+export function useTracker<TDataProps> (
+  reactiveFn: () => TDataProps
+): TDataProps
+export function useTracker<TDataProps> (
+  reactiveFn: () => TDataProps,
+  deps: React.DependencyList
+): TDataProps
+export function useTracker<TDataProps> (
   getMeteorData: () => TDataProps,
   deps: React.DependencyList,
   skipUpdate?: (prev: TDataProps, next: TDataProps) => boolean
-): TDataProps;
-export function useTracker<TDataProps>(
+): TDataProps
+export function useTracker<TDataProps> (
   getMeteorData: () => TDataProps,
   skipUpdate: (prev: TDataProps, next: TDataProps) => boolean
-): TDataProps;
+): TDataProps
 
-export function withTracker<TDataProps, TOwnProps>(
+export function withTracker<TDataProps, TOwnProps> (
   reactiveFn: (props: TOwnProps) => TDataProps
-): (reactComponent: React.ComponentType<TOwnProps & TDataProps>) => React.ComponentClass<TOwnProps>;
-export function withTracker<TDataProps, TOwnProps>(options: {
-  getMeteorData: (props: TOwnProps) => TDataProps;
-  pure?: boolean | undefined;
-  skipUpdate?: (prev: TDataProps, next: TDataProps) => boolean;
-}): (reactComponent: React.ComponentType<TOwnProps & TDataProps>) => React.ComponentClass<TOwnProps>;
+): (
+  reactComponent: React.ComponentType<TOwnProps & TDataProps>
+) => React.ComponentClass<TOwnProps>
+export function withTracker<TDataProps, TOwnProps> (options: {
+  getMeteorData: (props: TOwnProps) => TDataProps
+  pure?: boolean | undefined
+  skipUpdate?: (prev: TDataProps, next: TDataProps) => boolean
+}): (
+  reactComponent: React.ComponentType<TOwnProps & TDataProps>
+) => React.ComponentClass<TOwnProps>
 
-export function useSubscribe(name?: string, ...args: any[]): () => boolean;
+export function useSubscribe (name?: string, ...args: any[]): () => boolean
 
-// Suspense
-export function useSubscribe(name: string, ...params: EJSON[]) : Meteor.SubscriptionHandle;
-
-export function useFind<T>(factory: () => Mongo.Cursor<T>, deps?: React.DependencyList): T[];
-export function useFind<T>(factory: () => Mongo.Cursor<T> | undefined | null, deps?: React.DependencyList): T[] | null;
-
-// Suspense
-export function useFind<T>(collection : Mongo.Collection<T>, findArgs: Parameters<Mongo.Collection<T>['find']> | null): T[] | null
-
+export function useFind<T> (
+  factory: () => Mongo.Cursor<T>,
+  deps?: React.DependencyList
+): T[]
+export function useFind<T> (
+  factory: () => Mongo.Cursor<T> | undefined | null,
+  deps?: React.DependencyList
+): T[] | null
