9a49ac02161937fe8347522c98244334e55e7923
Gabriel Grubba <Grubba27@hotmail.com>
2023-03-08 16:34:17 -0300

    created suspense types

---
+++ packages/react-meteor-data/suspense/react-meteor-data.d.ts
@@ -0,0 +1,8 @@
+// Suspense
+import { type Mongo } from 'meteor/mongo'
+import { type EJSON } from 'meteor/ejson'
+import { type Meteor } from 'meteor/meteor'
+
+export function useFind<T> (collection: Mongo.Collection<T>, findArgs: Parameters<Mongo.Collection<T>['find']> | null): T[] | null
+
+export function useSubscribe (name: string, ...params: EJSON[]): Meteor.SubscriptionHandle
