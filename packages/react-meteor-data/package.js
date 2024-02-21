5e1c91d1fee2a571802af4dde2c5080871eabcfd
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-02-21 16:42:05 -0300

    update to use newer version of typescript

--- packages/react-meteor-data/package.js
+++ packages/react-meteor-data/package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'react-meteor-data',
   summary: 'React hook for reactively tracking Meteor data',
-  version: '3.0.0-beta300.0',
+  version: '3.0.0-beta300.1',
   documentation: 'README.md',
   git: 'https://github.com/meteor/react-packages'
 })
