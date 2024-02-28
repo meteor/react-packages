e845e88ed1c9e195215eaa67c5b6dd673dd12942
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-02-28 19:17:25 -0300

    udpated react accounts to beta.6

--- packages/react-meteor-accounts/package.js
+++ packages/react-meteor-accounts/package.js
@@ -3,13 +3,13 @@
 Package.describe({
   name: 'react-meteor-accounts',
   summary: 'React hooks and HOCs for reactively tracking Meteor Accounts data',
-  version: '1.0.0',
+  version: '1.0.1-beta300.1',
   documentation: 'README.md',
   git: 'https://github.com/meteor/react-packages',
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.10', '2.3']);
+  api.versionsFrom(['1.10', '2.3', '3.0-beta.6']);
 
   api.use(['accounts-base', 'tracker', 'typescript']);
 
