a6902d61e1832ec607dccd58f4313c5e359c22e8
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-04 22:54:52 +0200

    Add constraints for Meteor 3.0-rc.0 release

--- packages/react-meteor-state/package.js
+++ packages/react-meteor-state/package.js
@@ -3,13 +3,13 @@
 Package.describe({
   name: 'react-meteor-state',
   summary: 'React hook for reactively tracking Meteor data',
-  version: '1.0.0-beta.1',
+  version: '1.0.0-beta.2',
   documentation: 'README.md',
   git: 'https://github.com/meteor/react-packages',
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.10', '2.3']);
+  api.versionsFrom(['1.10', '2.3', '3.0-rc.0']);
   api.use('tracker');
   api.use('typescript');
 
